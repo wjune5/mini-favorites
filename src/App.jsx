b9f8d8c724061642e1adb362d26387682bf73c17
wjune <hesterfan@outlook.com>
2025-04-21 15:23:29 -0700

    [doc] README

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
   const dispatch = useDispatch();
   return (
     <>
-      <div className="pt-8 p-4 bg-white rounded shadow">
-        <h1 className="text-xl font-bold mb-4">迷你网页收藏夹</h1>
+      <div class="pt-8 p-4 bg-white rounded shadow">
+        <h1 class="text-xl font-bold mb-4">迷你网页收藏夹</h1>
         <AddFavoriteForm />
-        <button onClick={() => dispatch(clearFavorites())} className="px-4 text-center text-violet-600 border border-violet-600 rounded-full hover:bg-violet-600 hover:text-white active:bg-indigo-500 focus:outline-none focus:ring">清空</button>
+        <button onClick={() => dispatch(clearFavorites())} class="px-4 text-center text-violet-600 border border-violet-600 rounded-full hover:bg-violet-600 hover:text-white active:bg-indigo-500 focus:outline-none focus:ring">清空</button>
         <FavoriteList />
       </div>
     </>
--- src/components/AddFavoriteForm.jsx
+++ src/components/AddFavoriteForm.jsx
@@ -19,10 +19,10 @@ export default function AddFavoriteForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-x-2 mb-4">
+    <form onSubmit={handleSubmit} class="space-x-2 mb-4">
       <input value={title} onChange={e => setTitle(e.target.value)} placeholder="名称" class="border p-1 rounded-lg border-gray-300 focus:outline-none focus:border-violet-500"/>
-      <input value={url} onChange={e => setUrl(e.target.value)} placeholder="链接" className="border p-1 rounded-lg border-gray-300 focus:outline-none focus:border-violet-500"/>
-      <input value={tag} onChange={e => setTag(e.target.value)} placeholder="分类(可选)" className="border p-1 rounded-lg border-gray-300 focus:outline-none focus:border-violet-500"/>
+      <input value={url} onChange={e => setUrl(e.target.value)} placeholder="链接" class="border p-1 rounded-lg border-gray-300 focus:outline-none focus:border-violet-500"/>
+      <input value={tag} onChange={e => setTag(e.target.value)} placeholder="分类(可选)" class="border p-1 rounded-lg border-gray-300 focus:outline-none focus:border-violet-500"/>
       <button class="px-4 py-1 text-center text-white bg-violet-600 border border-violet-600 rounded-full active:text-violet-500 hover:bg-transparent hover:text-violet-600 focus:outline-none focus:ring" type="submit">添加</button>
     </form>
   );
